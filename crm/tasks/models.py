004425d19d80d73b3e5ea957771bc015f94572d3
luzanov99 <luzanov.zena@gmail.com>
2022-08-28 23:21:43 +0300

    add users to task models

--- crm/tasks/models.py
+++ crm/tasks/models.py
@@ -9,3 +9,12 @@ class Task(models.Model):
     date = models.DateField(verbose_name='Запланировано')
     create_date = models.DateTimeField(auto_now_add=True)
     priority = models.IntegerField(verbose_name='Приоритет')
+    remind = models.BooleanField(default=False)
+    remind_description = models.CharField(max_length=250, null=True, blank=True)
+    repeated_task = models.BooleanField(default=False)
+    repeated_task_days = models.PositiveIntegerField(blank=True, null=True)
+    related_task = models.ForeignKey('self', null=True, blank=True, on_delete=models.SET_NULL)
+    related_people = models.ManyToManyField(User, related_name='related_people', blank=True)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='created_by')
+    updated_by = models.ForeignKey(User, blank=True, null=True, on_delete=models.CASCADE, related_name='updated_by')
+    
